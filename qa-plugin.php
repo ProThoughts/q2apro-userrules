15182e0bd1caeaf7e7bf2e29827eebbab28f56a2
q2apro.com <info@q2apro.com>
2016-08-28 09:36:06 +0300

    corrected author

--- qa-plugin.php
+++ qa-plugin.php
@@ -6,7 +6,7 @@
 	Plugin Description: Set custom rules for editing
 	Plugin Version: 0.1
 	Plugin Date: 2015-07-16
-	Plugin Author: Question2Answer
+	Plugin Author: q2apro.com
 	Plugin Author URI: http://www.q2apro.com
 	Plugin License: GPLv2
 	Plugin Minimum Question2Answer Version: 1.6
